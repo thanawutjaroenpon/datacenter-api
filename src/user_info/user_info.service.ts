5a99aaa6d824df5a9fa65bec8e5f4dfe6664cf38
MKVIEW <mastertime7824@gmail.com>
2025-02-25 18:58:34 +0700

    fix update

--- src/user_info/user_info.service.ts
+++ src/user_info/user_info.service.ts
@@ -106,22 +106,22 @@ export class UserInfoService {
   }
 
   async updateByid_card(id_card: string, updateUserInfoDto: UpdateUserInfoDto) {
-    const userInfo = await this.userInfoRepository.findOne({ where: { id_card } });
+
+    console.log(id_card)
+    const userInfo = await this.userInfoRepository.findOne({ where: { id_card:id_card } });
+    
     if (!userInfo) {
       throw new NotFoundException(`UserInfo with ID #${id_card} not found`);
     }
-    if(updateUserInfoDto.id_card && updateUserInfoDto.id_card !== userInfo.id_card){
-      const existingUserid_card = await this.userInfoRepository.findOne({
-        where: {  id_card: updateUserInfoDto.id_card },
-      });
-      if (existingUserid_card) {
-        throw new NotFoundException(`UserInfo with Student ID #${updateUserInfoDto.id_card} already exists`);
-      }
-    }
-    await this.userInfoRepository.update(id_card, updateUserInfoDto);
 
-    return this.userInfoRepository.findOne({ where: { id_card } });
+    Object.assign(userInfo, updateUserInfoDto);
+    const updatedUserInfo = await this.userInfoRepository.save(userInfo);
+  
+  
+    return updatedUserInfo;
   }
+  
+  
 
 
   async Getprofile(currentUser:string){
