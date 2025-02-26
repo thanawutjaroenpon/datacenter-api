b7257e30a83ba840c7d12d5d029abf2c5aa484ca
MKVIEW <mastertime7824@gmail.com>
2025-02-27 03:31:19 +0700

    add getall nfclog

--- src/nfc_log/nfc_log.module.ts
+++ src/nfc_log/nfc_log.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NfcLogService } from './nfc_log.service';
 import { NfcLogController } from './nfc_log.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NfcLog } from './entities/nfc_log.entity';
+import { UserInfo } from '../user_info/entities/user_info.entity';
 
 @Module({
   controllers: [NfcLogController],
   providers: [NfcLogService],
+  imports: [TypeOrmModule.forFeature([NfcLog,UserInfo])]
 })
 export class NfcLogModule {}
--- src/nfc_log/nfc_log.service.ts
+++ src/nfc_log/nfc_log.service.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNfcLogDto } from './dto/create-nfc_log.dto';
 import { UpdateNfcLogDto } from './dto/update-nfc_log.dto';
+import { NfcLog } from './entities/nfc_log.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserInfo } from 'src/user_info/entities/user_info.entity';
 
 @Injectable()
 export class NfcLogService {
+    constructor(
+      @InjectRepository(NfcLog)
+      private nfclogrepository: Repository<NfcLog>,
+      @InjectRepository(UserInfo)
+      private userRepository: Repository<UserInfo>,
+  
+    ) {}
   create(createNfcLogDto: CreateNfcLogDto) {
     return 'This action adds a new nfcLog';
   }
 
-  findAll() {
-    return `This action returns all nfcLog`;
-  }
+  async findAll() {
+ 
+    const logs = await this.nfclogrepository.find();
+    
+  
+    const users = await this.userRepository.find();
+    
+    
+    const result = logs.map(log => {
+        const user = users.find(u => u.student_id === log.student_id);
+        return {
+            ...log,
+            firstname: user ? user.first_name : 'Unknown',
+            lastname: user ? user.last_name : 'Unknown'
+        };
+    });
+    
+    return result;
+}
 
   findOne(id: number) {
     return `This action returns a #${id} nfcLog`;
