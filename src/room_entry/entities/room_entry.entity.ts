0cd6efafb5cc87ee109b4a78dcf3cb6c9dac1ab7
sasipong jiteufeu <115165630+sasipongjiteufeu@users.noreply.github.com>
2025-02-27 17:35:25 +0700

    HOT FIX EVER HAVE

--- src/room_entry/entities/room_entry.entity.ts
+++ src/room_entry/entities/room_entry.entity.ts
@@ -1,26 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn, ViewEntity } from "typeorm";
 
-@Entity({ name: 'room_entry_method' })
-export class RoomEntry {
-    @PrimaryGeneratedColumn()
-    id: number;
 
-    @Column()
-    entry_time: string;
-  
-    @Column()
-    room_id: string;
-  
-    @Column()
-    student_id: string;
-  
-    @Column()
-    first_name: string;
-  
-    @Column()
-    position: string;
-  
-    @Column()
-    entry_method: string;}
 
 
