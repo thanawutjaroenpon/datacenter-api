1717ed6a0ee790b23706a7b135e27404227b16b4
MKVIEW <mastertime7824@gmail.com>
2025-02-27 23:22:09 +0700

    fix bug register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,30 +5,36 @@ import { Request } from 'express';
 import { JwtAuthGuard } from './guards/jwt.guard';
 import { ApiBearerAuth, ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@ApiBearerAuth('access-token')
+
 @Controller('auth')
 @ApiTags('Authorize')
 export class AuthController {
   constructor(private authService: AuthService) {}
-
-  @Post('GetToken')
-  getToken(): { token: string } {
-    const token = this.authService.validateUser();
-    return { token };
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('HelloWorld')
-  getHelloWorld(@Req() req: Request): string {
-    return this.authService.getHelloWorld();
-  }
   
+
   @Post('register')
   @ApiBody({ type: AuthPayloadDto })
   async register(@Body() authPayloadDto: AuthPayloadDto) {
     return this.authService.register(authPayloadDto.username, authPayloadDto.password);
   }
 
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Post('GetToken')
+  getToken(): { token: string } {
+    const token = this.authService.validateUser();
+    return { token };
+  }
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
+  @Get('HelloWorld')
+  getHelloWorld(@Req() req: Request): string {
+    return this.authService.getHelloWorld();
+  }
+  
+
+  @ApiBearerAuth('access-token')
+  @UseGuards(JwtAuthGuard)
   @Post('login')
   @ApiBody({ type: AuthPayloadDto })
   async login(@Body() authPayloadDto: AuthPayloadDto) {
